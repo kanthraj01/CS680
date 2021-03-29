91cc6051e7a342bd14b6299d6c49e07539f7ecc4
kanthraj01 <81532615+kanthraj01@users.noreply.github.com>
2021-03-28 22:33:33 -0400

    hw04

---
+++ HW04/src/edu/umb/cs680/hw04/Student.java
@@ -0,0 +1,86 @@
+package edu.umb.cs680.hw04;
+
+public class Student {
+	String name;
+	int i20number;
+	int yrsInState;
+	String usAddr;
+	String foreignAddr;
+	String priorStateAddr;
+	private StudentStatus status = null;
+	float tuition;
+
+	Student(String name, String usAddr, int yrsInState, int i20number, String foreignAddr, StudentStatus status) {
+		this.name = name;
+		this.usAddr = usAddr;
+		this.yrsInState = yrsInState;
+		this.i20number = i20number;
+		this.foreignAddr = foreignAddr;
+		this.status = status;
+	}
+
+	public float getTuition() {
+		return tuition; // returns tuition total
+	}
+
+	public void setTuition(float tuition) {
+		this.tuition = tuition; // sets tuition total
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int geti20number() {
+		return i20number;
+	}
+
+	public void seti20number(int i20number) {
+		this.i20number = i20number;
+	}
+
+	public int getYrsInState() {
+		return yrsInState;
+	}
+
+	public void setYrsInState(int yrsInState) {
+		this.yrsInState = yrsInState;
+	}
+
+	public String getUsAddr() {
+		return usAddr;
+	}
+
+	public void setUsAddr(String usAddr) {
+		this.usAddr = usAddr;
+	}
+
+	public String getForeignAddr() {
+		return foreignAddr;
+	}
+
+	public void setForeignAddr(String foreignAddr) {
+		this.foreignAddr = foreignAddr;
+	}
+
+	public String getPriorStateAddr() {
+		return priorStateAddr;
+	}
+
+	public void setPriorStateAddr(String priorStateAddr) {
+		this.priorStateAddr = priorStateAddr;
+	}
+
+	public StudentStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(StudentStatus status) {
+		this.status = status;
+	}
+
+}
---
+++ HW04/src/edu/umb/cs680/hw04/StudentStatus.java
@@ -0,0 +1,6 @@
+package edu.umb.cs680.hw04;
+
+public enum StudentStatus {
+
+	INSTATE, OUTSTATE, INTL
+}
